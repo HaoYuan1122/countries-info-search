428518fda1a5c218afd3ec3ba5608f0145bc82b8
Hao Yuan <haomichaelyuan@gmail.com>
2023-08-17 17:34:37 +1000

    router issue fixed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import HomePage from "./components/HomePage/HomePage";
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import CountryDetail from "./components/CountryDetail/CountryDetail";
 
 function App() {
   return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<HomePage/>}/>
-                <Route path='/:name' element={<CountryDetail/>}/>
-            </Routes>
-        </BrowserRouter>
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/"/>}/>
+        <Route path="/countries-info-search" element={<HomePage />} />
+        <Route path="/:name" element={<CountryDetail />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
